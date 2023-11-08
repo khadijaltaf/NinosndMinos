dfcb3ea166cafe4b69dd1af595d5c21fb4664c9c
khadijaltaf <khadijaltaf008@gmail.com>
2023-11-08 18:56:56 +0500

    nav

--- Frontend/ninosndminos/src/App.jsx
+++ Frontend/ninosndminos/src/App.jsx
@@ -3,6 +3,7 @@ import Nav from './components/nav/Nav1.jsx'
 import ErrorBoundary from './ErrorBoundary.jsx'
 import './index.css'
 import Login from "./Components/Login/Login.jsx"
+import Homepage from "./Components/HomePage/Homepage.jsx"
 
 
 function App() {
--- Frontend/ninosndminos/src/Components/Navbar/Nav1.jsx
+++ Frontend/ninosndminos/src/Components/Navbar/Nav1.jsx
@@ -1,47 +1,107 @@
-import React from 'react'
+import React from "react";
 // import './Nav1.js'
-import './Nav1.css'
-import logo from '../../assets/blacklogo.png'
+import "./Nav1.css";
+import logo from "../../assets/blacklogo.png";
+import Homepage from "../HomePage/Homepage.jsx";
 
 const Nav1 = () => {
   return (
-      <div>
-            
-            <section className="nav__side">
-                <nav>
-                    {/* Sidebar */}
+    <>
+      <nav class="navbar nav bg-body-tertiary fixed-top transparent">
+        <div class="container-fluid">
+          <button
+            // class="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
+            aria-label="Toggle navigation"
+          >
+            <i class="fa-solid fa-bars fa-2xl size"></i>
+          </button>
 
-                    <div className="links">
-                      <button >
-                      <i class="fa-solid fa-bars menu"></i>
-                      <i class="fa-solid fa-magnifying-glass"></i>
-                      </button>
-                    </div>
-                    {/* sidebar */}
-                    
-                    <div className="barand">
-                        <img src={logo} alt="" />
-                    </div>
-                    <div className="links">
-                        
-                        <div className="links">
-                          <button>
-                            <i className="fa-solid fa-cart-shopping menu"></i>
-                            <i className="fa-regular fa-circle-user "></i>
-                            </button>
-                        </div>
-                    </div>
-                    <div className="profilre_dropdown">
-                        <ul>
-                            <li><a href="#">My Profile</a></li>
-                            <li><a href="#">Sign in</a></li>
-                        </ul>
-                    </div>
-                </nav>
-                
-            </section>
-    </div>
-  )
-}
+          <div
+            class="offcanvas offcanvas-start"
+            tabindex="-1"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+          >
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+                Offcanvas
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    Link
+                  </a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Dropdown
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <hr class="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <form class="d-flex mt-3" role="search">
+                <input
+                  class="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+              </form>
+            </div>
+          </div>
+          
+          
+          {/* <a class="navbar-brand" href="#">
+            <img className="img" src={logo} alt="" />
+          </a> */}
+        </div>
+      </nav>
 
-export default Nav1
+      <Homepage/>
+    </>
+  );
+};
+
+export default Nav1;
